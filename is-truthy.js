e21c5898064b8b2c74fa6748ca0013a778345f15
“Rin” <renrust26@gmail.comgit config --global user.name Ringit config --global user.email renrust26@gmail.com>
2023-04-21 17:55:29 -0700

    step 2 completed of workshop

---
+++ is-truthy.js
@@ -0,0 +1,12 @@
+// If the value is truthy, print true;
+// If the value is falsy, print the corresponding result:
+//          "The boolean value false is falsy";
+//          "The null value is falsy";
+//          "undefined is falsy";
+//          "The number 0 is falsy (the only falsy number)";
+//          "The empty string is falsy (the only falsy string)";
+
+
+//value is "I am a string" with the print saying: "true"
+//value is "false" with the print saying: "The boolean value false is falsy"
+
