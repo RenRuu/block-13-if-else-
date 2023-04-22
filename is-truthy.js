9f8e4e5872ab64be800769338ef1f8c23f50cd94
“Rin” <renrust26@gmail.comgit config --global user.name Ringit config --global user.email renrust26@gmail.com>
2023-04-21 18:36:22 -0700

    Is truthy done

--- is-truthy.js
+++ is-truthy.js
@@ -7,6 +7,22 @@
 //          "The empty string is falsy (the only falsy string)";
 
 
-//value is "I am a string" with the print saying: "true"
-//value is "false" with the print saying: "The boolean value false is falsy"
+let fish = ["I am a string", false, null, undefined, 0, ""];
+
+if (fish) {
+    console.log(true);
+} else if (fish === false) {
+    console.log("The boolean values false is falsy");
+} else if (fish === null) {
+    console.log("The null values is falsy");
+} else if (fish === 0) {
+    console.log("The number 0 is falsy (the only falsy number)");
+} else if (fish === "") {
+    console.log("The empty string is falsy (the only falsy string)");
+} else if (fish === undefined) {
+    console.log("undefined is falsy");
+}
+
+
+
 
