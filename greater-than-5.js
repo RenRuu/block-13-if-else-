b0fe74c1b8ee6331c1af7467c6ae296b73da5777
“Rin” <renrust26@gmail.comgit config --global user.name Ringit config --global user.email renrust26@gmail.com>
2023-04-24 15:02:31 -0700

    Completed "Greater than 5"

---
+++ greater-than-5.js
@@ -0,0 +1,64 @@
+// Using if/else statements, JavaScript code in a script tag that prints true if both parameters are greater or equal to 5.
+
+
+// Showing true; both parameters are greater or equal to 5.
+const a = 5;
+const b = 6;
+
+if (a >= 5 && b >= 5) {
+    console.log(true);
+} else {
+    console.log(false);
+}
+
+// Showing true; both parameters are greater or equal to 5.
+const a = 10;
+const b = 11;
+
+if (a >= 5 && b >= 5) {
+    console.log(true);
+} else {
+    console.log(false);
+}
+
+// Showing false; both parameters are less than 5.
+const a = 0;
+const b = 0;
+
+if (a >= 5 && b >= 5) {
+    console.log(true);
+} else {
+    console.log(false);
+}
+
+// Showing false to this statement "both parameters are greater or equal to 5".
+// Only "a" exceeds 5 while "b" is less than 5.
+const a = 1000;
+const b = -1000;
+
+if (a >= 5 && b >= 5) {
+    console.log(true);
+} else {
+    console.log(false);
+}
+
+// Showing false to this statement "both parameters are greater or equal to 5".
+// Only "a" exceeds 5 while "b" is less than 5.
+const a = 6;
+const b = 4;
+
+if (a >= 5 && b >= 5) {
+    console.log(true);
+} else {
+    console.log(false);
+}
+
+// Showing true; both parameters are greater or equal to 5.
+const a = 5;
+const b = 5;
+
+if (a >= 5 && b >= 5) {
+    console.log(true);
+} else {
+    console.log(false);
+}
