944ee5e71e00c5788ab4f5f722b4ca1d34b0d491
“Rin” <renrust26@gmail.comgit config --global user.name Ringit config --global user.email renrust26@gmail.com>
2023-04-24 15:31:02 -0700

    Completed "Number Line"

--- number-line.js
+++ number-line.js
@@ -16,41 +16,41 @@ if (num1 + num2 >= 100) {
 } 
 
 // Showing sum is larger than 0
-let num1 = 99;
-let num2 = -2;
+let num3 = 99;
+let num4 = -2;
 
-if (num1 + num2 >= 0) {
+if (num3 + num4 >= 0) {
     console.log("97 is greater than 0");
 }
 
 // Showing sum is greater than 100 (again)
-let num1 = 0;
-let num2 = 101;
+let num5 = 0;
+let num6 = 101;
 
-if (num1 + num2 >= 100) {
+if (num5 + num6 >= 100) {
     console.log("101 is greater than 100");
 }
 
 // Showing sum is equal to 0
-let num1 = 500;
-let num2 = -500;
+let numS = 500;
+let numSS = -500;
 
-if (num1 + num2 = 0) {
+if (numS + numSS == 0) {
     console.log("0 is equal to 0");
 }
 
 // Showing sum is less than -1000
-let num1 = -1000;
-let num2 = 0;
+let num9 = -1000;
+let num10 = 0;
 
-if (num1 + num2 === -1000 ) {
+if (num9 + num10 === -1000 ) {
     console.log("-1000 is a negative number");
 }
 
 // Showing sum is a negative number
-let num1 = -5;
-let num2 = 0;
+let num11 = -5;
+let num12 = 0;
 
-if (num1 + num2 === -5 ) {
+if (num11 + num12 === -5 ) {
     console.log("-5 is a negative number");
 }
