a5a6862e5ad4b84f089bffa3cb9af30cb16848a6
“Rin” <renrust26@gmail.comgit config --global user.name Ringit config --global user.email renrust26@gmail.com>
2023-04-24 12:52:55 -0700

    Completed "Number Line"

---
+++ number-line.js
@@ -0,0 +1,56 @@
+// Using If/Else statements:
+// JavaScript code in a script tag that takes the sum of two numbers and prints the corresponding result:
+//                  sum + " is less than -1000"; -
+//                  sum + " is a negative number"; 
+//                  sum + " is equal to 0"; -
+//                  sum + " is larger than 0"; -
+//                  sum + " is greater than 100"; -
+
+
+// Showing sum is greater than 100
+let num1 = 50;
+let num2 = 51;
+
+if (num1 + num2 >= 100) {
+    console.log("101 is greater than 100");
+} 
+
+// Showing sum is larger than 0
+let num1 = 99;
+let num2 = -2;
+
+if (num1 + num2 >= 0) {
+    console.log("97 is greater than 0");
+}
+
+// Showing sum is greater than 100 (again)
+let num1 = 0;
+let num2 = 101;
+
+if (num1 + num2 >= 100) {
+    console.log("101 is greater than 100");
+}
+
+// Showing sum is equal to 0
+let num1 = 500;
+let num2 = -500;
+
+if (num1 + num2 = 0) {
+    console.log("0 is equal to 0");
+}
+
+// Showing sum is less than -1000
+let num1 = -1000;
+let num2 = 0;
+
+if (num1 + num2 === -1000 ) {
+    console.log("-1000 is a negative number");
+}
+
+// Showing sum is a negative number
+let num1 = -5;
+let num2 = 0;
+
+if (num1 + num2 === -5 ) {
+    console.log("-5 is a negative number");
+}
